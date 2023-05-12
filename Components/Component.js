3a8f79d57a204320a215273d2dfabb9397fa5f5d
unknown <brandobogar@gmail.com>
2023-05-12 12:59:41 +0800

    exporting component as one line

---
+++ Components/Component.js
@@ -0,0 +1,27 @@
+// export { default as Project } from "./project/project";
+// export { default as About } from "./about/About";
+// export { default as Education } from "./education/Education";
+// export { default as Footer } from "./footer/Footer";
+// export { default as Skills } from "./skills/Skills";
+// export { default as Social } from "./social/Social";
+// export { default as Nav } from "./Navbar/Nav";
+// In Components.js
+import Project from "./project/project";
+import Education from "./education/Education";
+import Social from "./social/Social";
+import About from "./about/About";
+import Skills from "./skills/Skills";
+import Footer from "./footer/Footer";
+import Nav from "./Navbar/Nav";
+
+const Components = {
+  Project,
+  Education,
+  Social,
+  About,
+  Skills,
+  Footer,
+  Nav,
+};
+
+export default Components;
