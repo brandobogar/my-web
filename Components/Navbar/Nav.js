46cbe6b25147ecc50b56d426cc82f7b736ce9061
unknown <brandobogar@gmail.com>
2023-05-12 12:53:54 +0800

    add dropdown to features sub menu

--- Components/Navbar/Nav.js
+++ Components/Navbar/Nav.js
@@ -1,10 +1,13 @@
+//! komponen navbar boiler
+
 import React from "react";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
-export default function Navbar({ fixed }) {
+export default function NavDump({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const [showFeature, setShowFeature] = React.useState(false);
   return (
     <>
       <nav className="fixed w-full flex flex-wrap items-center justify-between px-2 py-3 bg-myteal mb-3 font-oxygen text-myblack text-xl font-medium shadow-xl">
@@ -60,13 +63,30 @@ export default function Navbar({ fixed }) {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link
-                  className="px-3 py-2 flex items-center    leading-snug  hover:opacity-75"
-                  href="/features"
-                  scroll={false}
+                <button
+                  className="px-3 py-2 flex items-center    leading-snug  "
+                  onClick={() => setShowFeature(!showFeature)}
                 >
                   <span className="ml-2">Features</span>
-                </Link>
+                  <ul
+                    className={
+                      " text-xl " +
+                      (showFeature
+                        ? "absolute flex flex-col h-auto w-40 divide-y-2 bg-slate-50 justify-evenly items-center rounded-lg z-50 top-16 right-1 shadow-2xl"
+                        : "hidden")
+                    }
+                  >
+                    <li className="flex flex-col justify-center items-center  w-full p-2">
+                      <Link href={"/features/lyrics"}>Lyrics</Link>
+                    </li>
+                    <li className="flex flex-col justify-center items-center  w-full p-2">
+                      <Link href={"/features/sentiment"}>Sentiment</Link>
+                    </li>
+                    <li className="flex flex-col justify-center items-center  w-full p-2">
+                      <Link href={"/features/todo"}>Todo</Link>
+                    </li>
+                  </ul>
+                </button>
               </li>
             </ul>
           </div>
