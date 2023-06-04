0c23ed74ad4ae75c757fde8c1d1c40161c24a014
unknown <brandobogar@gmail.com>
2023-06-05 02:03:38 +0800

    add emoji to represent the sentiment score

--- src/pages/features/sentiment/index.js
+++ src/pages/features/sentiment/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Layout from "../../../../Components/features/layout/layout";
 import MyHead from "../../../../Components/features/head/MyHead";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFaceFrown, faFaceSmile } from "@fortawesome/free-solid-svg-icons";
-import { faFaceMeh } from "@fortawesome/free-regular-svg-icons";
+
+import Image from "next/image";
+
 import UI from "../../../../Components/ui/UI";
 
 export default function _sentiment() {
   const [text, setText] = React.useState("");
-  const [result, setResult] = React.useState("");
-
+  // const [result, setResult] = React.useState("");
+  const [score, setScore] = React.useState(0);
   const handleTextChange = (event) => {
     setText(event.target.value);
   };
@@ -18,7 +18,35 @@ export default function _sentiment() {
     var Sentiment = require("sentiment");
     var sentiment = new Sentiment();
     const res = sentiment.analyze(text);
-    setResult(`Score: ${res.score}, Comparative: ${res.comparative}`);
+    // setResult(`Score: ${res.score}, Comparative: ${res.comparative}`);
+    console.log({ res });
+    setScore(res.score);
+  };
+
+  const SetEmoji = () => {
+    if (score == 0) {
+      return (
+        <Image src="/icons/netral.svg" alt="netral" width={100} height={100} />
+      );
+    } else if (score > 0) {
+      return (
+        <Image
+          src="/icons/positif.svg"
+          alt="positif"
+          width={100}
+          height={100}
+        />
+      );
+    } else if (score < 0) {
+      return (
+        <Image
+          src="/icons/negatif.svg"
+          alt="negatif"
+          width={100}
+          height={100}
+        />
+      );
+    }
   };
 
   return (
@@ -29,18 +57,19 @@ export default function _sentiment() {
         author={"Brando Margendy Bogar"}
         keyword={"sentiment, sentiment analyis, node js"}
       />
-      <div className="flex flex-col items-center justify-center w-full h-screen gap-10">
+      <div className="flex flex-col items-center  w-full h-screen gap-10 mt-20">
         <h1 className="text-3xl text-myblack">Sentiment analysis</h1>
 
         <textarea
           value={text}
           onChange={handleTextChange}
           placeholder="Enter a sentences"
-          className="bg-white w-48 text-myblack h-24 p-2 resize-none"
+          className="w-2/4 h-32 px-2 py-1 text-sm bg-white border-0 rounded shadow outline-none resize-none placeholder-slate-300 text-slate-600 focus:outline-none focus:ring"
         ></textarea>
         <UI.Button type={"button"} onClick={handleAnalyse} title={"Analyze"} />
+        <SetEmoji />
 
-        <p className="text-myblack">{result}</p>
+        {/* <p className="text-myblack">{result}</p> */}
       </div>
     </Layout>
   );
