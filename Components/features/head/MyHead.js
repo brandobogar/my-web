312a89eb25ba1088005515e25bdcd1a22bd1324e
unknown <brandobogar@gmail.com>
2023-06-04 12:28:27 +0800

    updated meta tag

--- Components/features/head/MyHead.js
+++ Components/features/head/MyHead.js
@@ -6,6 +6,10 @@ function MyHead({ pageTitle, desc, author, keyword }) {
       <meta name="keywords" content={keyword} />
       <meta name="description" content={desc} />
       <meta name="author" content={author} />
+      <meta
+        name="format-detection"
+        content="telephone=no, date=no, email=no, address=no"
+      />
       <title>{pageTitle} | Brando Bogar&apos;s Website</title>
     </Head>
   );
